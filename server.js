e4327cf2e7642f78d5cc07ad1faaf88fe0ac38ee
LEBRONGOAT2323 <hrsipes22@gmail.com>
2026-03-03 19:23:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const { WebSocketServer } = require("ws");
 
@@ -8,25 +7,69 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 const server = app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 );
 
 const wss = new WebSocketServer({ server });
 
 const players = {};
+let gameStarted = false;
+
+function assignRoles() {
+  const ids = Object.keys(players);
+  if (ids.length < 2) return;
+
+  const impostorId = ids[Math.floor(Math.random() * ids.length)];
+
+  ids.forEach(id => {
+    players[id].role = id === impostorId ? "impostor" : "crewmate";
+  });
+
+  gameStarted = true;
+
+  broadcast({ type: "rolesAssigned", players });
+}
 
 wss.on("connection", (ws) => {
-  const id = Date.now();
-  players[id] = { x: 100, y: 100 };
+  const id = Date.now().toString();
 
-  ws.send(JSON.stringify({ type: "init", id, players }));
+  players[id] = {
+    x: 100,
+    y: 100,
+    role: null,
+    alive: true
+  };
+
+  ws.send(JSON.stringify({
+    type: "init",
+    id,
+    players
+  }));
+
+  if (Object.keys(players).length >= 2 && !gameStarted) {
+    assignRoles();
+  }
 
   ws.on("message", (msg) => {
     const data = JSON.parse(msg);
 
-    if (data.type === "position") {
-      players[id] = data.position;
-      broadcast({ type: "update", id, position: data.position });
+    if (data.type === "position" && players[id]?.alive) {
+      players[id].x = data.position.x;
+      players[id].y = data.position.y;
+      broadcast({ type: "update", id, position: players[id] });
+    }
+
+    if (data.type === "kill") {
+      const targetId = data.targetId;
+
+      if (
+        players[id].role === "impostor" &&
+        players[targetId] &&
+        players[targetId].alive
+      ) {
+        players[targetId].alive = false;
+        broadcast({ type: "killed", targetId });
+      }
     }
   });
 
@@ -37,13 +80,9 @@ wss.on("connection", (ws) => {
 });
 
 function broadcast(msg) {
-  wss.clients.forEach((c) => {
+  wss.clients.forEach(c => {
     if (c.readyState === 1) {
       c.send(JSON.stringify(msg));
     }
   });
 }
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
